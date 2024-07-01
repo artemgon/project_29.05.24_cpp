518a139d4d7f67a170452472cd50f51639bfdca7
artemgon <artemhoncharuk123@gmail.com>
2024-07-01 15:15:32 +0300

    Developer modifying,

---
+++ lib.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <conio.h>
+#include <windows.h>
+#include <cmath>
+#include <stdlib.h>
+#include <time.h>
+#include <cstdarg>
+
+using namespace std;
