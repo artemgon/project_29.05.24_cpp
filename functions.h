e0ae6d10faa662d0fb8164a6093fd4a0c09973e0
artemgon <artemhoncharuk123@gmail.com>
2024-07-02 15:30:28 +0300

    Creating the 2nd task of 2hw.

--- functions.h
+++ functions.h
@@ -1,2 +1,9 @@
 #include "lib.h" 
 
+template<class T1, class T2>
+T2 Gcd(T1 a, T2 b)
+{
+    if (a == 0)
+        return b;
+    return Gcd(b % a, a);
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "functions.h"
 
 int main() {
+	cout << Gcd(196, 84);
 	return 0;
 }
