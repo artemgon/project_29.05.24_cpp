321afc069214cda6f5b8044a4727baeccf392e73
artemgon <artemhoncharuk123@gmail.com>
2024-07-02 09:47:33 +0300

    Creating the 2nd task of 1st hw.

--- functions.h
+++ functions.h
@@ -1,2 +1,36 @@
 #include "lib.h" 
 
+template<class T>
+void selectSort(T a[], long size) {
+	long i, j, k;
+	T x;
+	for (i = 0; i < size; i++) {
+		k = i;
+		x = a[i];
+		for (j = i + 1; j < size; j++)
+			if (a[j] < x) {
+				k = j;
+				x = a[j];
+			}
+		if (k != i) {
+			a[k] = a[i];
+			a[i] = x;
+		}
+	}
+}
+
+template<class T>
+T BinarySearch(T A[], int Lb, int Ub, T Key) {
+	int M;
+	while (1) {
+		M = (Lb + Ub) / 2;
+		if (Key < A[M])
+			Ub = M - 1;
+		else if (Key > A[M])
+			Lb = M + 1;
+		else
+			return M;
+		if (Lb > Ub)
+			return -1;
+	}
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include "functions.h"
 
 int main() {
+	srand(time(NULL));
+	const long SIZE = 10;
+	int ar[SIZE], key, index;
+	for (int i = 0; i < SIZE; i++) {
+		ar[i] = rand() % 100;
+		cout << ar[i] << "\t";
+	}
+	cout << "\n\n";
+	selectSort(ar, SIZE);
+	for (int i = 0; i < SIZE; i++) {
+		cout << ar[i] << "\t";
+	}
+	cout << "\n\n";
+	cout << "Enter any digit:";
+	cin >> key;
+	index = BinarySearch(ar, 0, SIZE, key);
+	cout << "Index - " << index << "\t";
+	cout << "\n\n";
 	return 0;
 }
